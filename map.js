95f58317d7ed9941364bce3fbc81cd6ab35f0e1c
awesomebun <56407164+awesomebun@users.noreply.github.com>
2019-11-21 12:03:53 -0500

    Map page
    with css and js

---
+++ map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXdlc29tZWJhbyIsImEiOiJjazF2MHZjbjUwNTJxM21temxxMWJ3dnVtIn0.KZJXbm4hhLg6GZR-cRyCaA';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/awesomebao/ck38cni8a24kb1cpbx6gmmahm'
+});
